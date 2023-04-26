00c40e02e3571af8d6ddd0bb930ba88f68ece933
Hyunwook Jo <acerghjk@gmail.com>
2023-04-26 17:45:33 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -21,20 +21,20 @@ def save_images(images, path, **kwargs):
     im.save(path)
 
 
-def get_data(args):
+def get_data(image_size, dataset_path, batch_size):
     transforms = torchvision.transforms.Compose([
         torchvision.transforms.Resize(80),  # args.image_size + 1/4 *args.image_size
-        torchvision.transforms.RandomResizedCrop(args.image_size, scale=(0.8, 1.0)),
+        torchvision.transforms.RandomResizedCrop(image_size, scale=(0.8, 1.0)),
         torchvision.transforms.ToTensor(),
         torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
-    dataset = torchvision.datasets.ImageFolder(args.dataset_path, transform=transforms)
-    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+    dataset = torchvision.datasets.ImageFolder(dataset_path, transform=transforms)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
     return dataloader
 
 
-def setup_logging(run_name):
+def setup_logging(cfg): 
     os.makedirs("models", exist_ok=True)
     os.makedirs("results", exist_ok=True)
-    os.makedirs(os.path.join("models", run_name), exist_ok=True)
-    os.makedirs(os.path.join("results", run_name), exist_ok=True)
+    os.makedirs(os.path.join("/content/drive/MyDrive/Conquer_Diffusion/Diff/models", cfg['DATA']['run_name']), exist_ok=True)
+    os.makedirs(os.path.join("/content/drive/MyDrive/Conquer_Diffusion/Diff/results", cfg['DATA']['run_name']), exist_ok=True)
